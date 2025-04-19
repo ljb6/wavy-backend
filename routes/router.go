c28d0bbf8cacaaf60fce8538da89967a137e7b3a
Lucca <luccajbecker@gmail.com>
2025-04-18 21:58:45 -0300

    feat: add /status route

--- routes/router.go
+++ routes/router.go
@@ -2,12 +2,19 @@ package router
 
 import (
 	"database/sql"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeServer(db *sql.DB) {
 	router := gin.Default()
+
+	router.GET("/status", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "working",
+		})
+	})
 
 	InitializeRoutes(router, db)
 
