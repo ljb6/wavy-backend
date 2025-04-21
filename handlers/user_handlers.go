7a8d2c1d0e27d71c1ce6cfc583656c0b81ad9314
Lucca <luccajbecker@gmail.com>
2025-04-20 22:27:47 -0300

    feat: add LoginUser handler

--- handlers/user_handlers.go
+++ handlers/user_handlers.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ljb6/wavy-backend/database"
 	"github.com/ljb6/wavy-backend/models"
+	"github.com/ljb6/wavy-backend/security"
 )
 
 func RegisterUser(c *gin.Context, db *sql.DB) {
@@ -28,4 +31,40 @@ func RegisterUser(c *gin.Context, db *sql.DB) {
 	}
 
 	c.JSON(http.StatusCreated, gin.H{"message": "User created with success", "id": userID})
+}
+
+func LoginUser(c *gin.Context, db *sql.DB) {
+	var user models.User
+
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed in login"})
+		return
+	}
+
+	var hashedPassword string
+	var userID int
+
+	query := "SELECT id, password FROM users WHERE email = $1"
+	err = db.QueryRow(query, user.Email).Scan(&userID, &hashedPassword)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User not found in database"})
+		return
+	}
+
+	if security.CheckPassword(hashedPassword, user.Password) {
+
+		token, err := security.GenerateJWT(strconv.Itoa(userID))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error in generating JWT token"})
+			return
+		}
+
+		c.SetCookie("token", token, 3600, "/", "", false, true)
+
+		c.Writer.Header().Set("Set-Cookie", fmt.Sprintf("token=%s; Path=/; HttpOnly; SameSite=None", token))
+
+		c.JSON(http.StatusOK, gin.H{"message": "User logged with success", "token": token})
+		return
+	}
 }
