79a47135174bbca32dcb5dbfb5e80c8fa55c7dfa
Lucca <luccajbecker@gmail.com>
2025-04-21 14:57:19 -0300

    feat: add Login service

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"errors"
+
 	"github.com/ljb6/wavy-backend/security"
 )
 
@@ -21,4 +23,17 @@ func (s *Service) Register(user User) error {
 
     user.Password = hashedPassword
     return s.repo.Create(user)
+}
+
+func (s *Service) Login(email, password string) (*User, error) {
+	user, err := s.repo.GetUserByEmail(email)
+	if err != nil {
+		return nil, errors.New("User not found")
+	}
+
+	if !security.CheckPassword(user.password, password) {
+		return nil, errors.New("Incorrect password")
+	}
+
+	return user, nil
 }
