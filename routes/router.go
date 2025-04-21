632975a7c81c103a5838ca6539b41f14fc2e2e5c
Lucca <luccajbecker@gmail.com>
2025-04-20 21:48:43 -0300

    feat: add cors config

--- routes/router.go
+++ routes/router.go
@@ -3,12 +3,24 @@ package router
 import (
 	"database/sql"
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitializeServer(db *sql.DB) {
 	router := gin.Default()
+
+	// Allows to receives request from different origins
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},  // frontend
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		ExposeHeaders:     []string{"Content-Length"},
+		MaxAge:            12 * time.Hour,
+	}))
 
 	router.GET("/status", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
