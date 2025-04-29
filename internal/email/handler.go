b2a462ef413baa6e5971f1404dcd105ae1dcca07
Lucca <luccajbecker@gmail.com>
2025-04-29 10:40:17 -0300

    feat: add SendEmailHandler func

--- internal/email/handler.go
+++ internal/email/handler.go
@@ -1,9 +1,33 @@
 package email
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type EmailHandler struct {
 	emailService *EmailService
 }
 
 func NewHandler(service *EmailService) *EmailHandler {
 	return &EmailHandler{emailService: service}
+}
+
+func (e *EmailHandler) SendEmailHandler(c *gin.Context) {
+	
+	userID := c.GetString("userID")
+
+	var req EmailReq
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid data"})
+	}
+
+	err := e.emailService.SendEmail(req, userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "server error"})
+	}
+
+	c.JSON(http.StatusOK, g.H{"message": "email sent"})
 }
